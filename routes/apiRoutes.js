38f37c7b935d47c63cbca5479bc21e9697105098
Naomi <n.valdovinos74@gmail.com>
2021-08-01 23:49:21 -0400

    almost finished, need to do api route and will be done

---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+//requires db.json to store note data that is being saved.
+// fs is used to store and retrieve notes
+const DbFile = require('../db/db.json');
+const fs = require('fs');
+
--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express();
 
 //Sets initial port. 
 const PORT = process.env.PORT || 3550;
-// const path = require('path');
+const path = require('path');
 
 // Express app is set to handle data parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
 //points our sever to a series of route files.
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
 
 //starts our server and allwos a link to the port 
 
